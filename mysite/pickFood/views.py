727f7acc287b12d5d8422a0c75830779e6702e59
eunag63 <kibm63@naver.com>
2021-06-16 21:41:24 +0900

    데이터 보여주기

--- mysite/pickFood/views.py
+++ mysite/pickFood/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import Food
+
 def index(request):
-    return HttpResponse("Hello world")
+    foods = Food.objects.all()
+    str = ''
+    for food in foods:
+        str += "<p>{}<br>".format(food.name)
+        str += food.introduction+"</p>"
+    return HttpResponse(str)
