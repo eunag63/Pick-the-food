cc331949e041d979f7471bc94c7cb0c2cfe5014c
eunag63 <eunag39@gmail.com>
2021-07-01 17:48:33 +0900

    admin.py Count 등록

--- mysite/pickFood/admin.py
+++ mysite/pickFood/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Food
+from .models import Food, Count
 
 
 admin.site.register(Food)
+admin.site.register(Count)
